3a88eff4bee08bbb80c6c7e72958581df64f0b7d
EZE CHINEDUM <ezechinedum504@gmail.com>
2023-08-27 19:32:07 +0000

    Updated server.js

--- app/server.js
+++ app/server.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello My Love!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`My app is listening on port ${port}`)
 })
